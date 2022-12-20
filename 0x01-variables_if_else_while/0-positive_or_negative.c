1e3dce3a075f56d5bb06abdfd44efcb22ff2f45f
Atkison1313 <kosma4smartpayz9ja@gmail.com>
2022-12-19 23:35:07 -0800

    to print positive or negative integer

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,11 +4,18 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: positive or negative 
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int n;
 
-	srand (time(0));
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 		if (n > 0)
