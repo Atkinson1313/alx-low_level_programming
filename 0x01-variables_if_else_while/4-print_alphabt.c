481816bacf3eb54f435bbbf0dd7b6576858f6576
Atkison1313 <kosma4smartpayz9ja@gmail.com>
2022-12-21 17:55:41 -0800

    print and ommit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * Description: prints all letters aprt from q and e
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z')
+		{
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
