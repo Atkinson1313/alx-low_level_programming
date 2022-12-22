e633c0d49c1ecd54a23fb985589f598405a26be8
Atkison1313 <kosma4smartpayz9ja@gmail.com>
2022-12-21 20:07:07 -0800

    print letters in reverse order

---
+++ 0x01-variables_if_else_while/.7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - statrt point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while
+		(c >= 'a')
+		{
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+
+}
